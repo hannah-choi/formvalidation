393ccc362ec26581de43d8238fccc263c48caf94
hannah-developer <hannahchoi.developer@gmail.com>
2021-01-28 22:56:55 +0000

    refactoring & added reset and alert

--- script.js
+++ script.js
@@ -5,69 +5,75 @@ const email = form.email;
 const password1 = form.password1;
 const password2 = form.password2;
 const formControls = document.querySelectorAll(".form-control");
+const success = document.getElementsByClassName("success");
 
-const showMessage = (input, message) => {
-    input.parentElement.classList.add("error");
-    input.parentElement.lastElementChild.textContent = message;
+const isRequired = elements => {
+    elements.map(el => {
+        if (!el.value) {
+            showError(el, ` is required`);
+        }
+    });
+};
+
+const emailCheck = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    re.test(email.value) ? showSuccess(email) : showError(email, " is invalid");
+};
+
+const showError = (el, message) => {
+    el.parentElement.classList.remove("success");
+    el.parentElement.classList.add("error");
+    el.parentElement.querySelector("small").innerHTML =
+        `${getName(el.name)}` + message;
     setTimeout(() => {
-        input.parentElement.classList.remove("error");
+        el.parentElement.classList.remove("error");
     }, 3000);
 };
 
-const showSuccess = input => {
-    input.parentElement.classList.add("success");
-};
-
-const checkEmail = input => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value)) {
-        showSuccess(input);
-    } else {
-        showMessage(input, "Email format is invalid");
+const showSuccess = el => {
+    if (!el.parentElement.classList.contains("error")) {
+        el.parentElement.classList.add("success");
     }
 };
 
-const checkLength = (input, min, max) => {
-    if (input.value.length < min || input.value.length > max) {
-        showMessage(
-            input,
-            `${getName(input)} should be in between 4 - 12 characters`
-        );
+const lengthCheck = (el, min, max) => {
+    if (el.value.length < min) {
+        showError(el, ` should be longer than ${min}`);
+    } else if (el.value.length > max) {
+        showError(el, ` shouldn't be longer than ${max}`);
     } else {
-        showSuccess(input);
+        el.parentElement.classList.add("success");
     }
 };
 
-const checkPw = (password1, password2) => {
-    if (password1.value !== password2.value) {
-        showMessage(password1, "Password aren't matching");
+const passwordMatch = (password1, password2) => {
+    if (password1.value && password1.value !== password2.value) {
+        showError(password1, " does not match");
     } else {
         showSuccess(password1);
     }
 };
 
-const isRequired = inputArray => {
-    inputArray.map(input => {
-        if (!input.value) {
-            input.parentElement.classList.add("error");
-            input.parentElement.lastElementChild.textContent = `${getName(
-                input
-            )} is required`;
-            setTimeout(() => {
-                input.parentElement.classList.remove("error");
-            }, 4000);
-        }
-    });
-};
-
-const getName = input => {
-    return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+const getName = name => {
+    if (name === password1 || name === password2) {
+        return Password;
+    }
+    return name.charAt(0).toUpperCase() + name.slice(1);
 };
 
 submitButton.addEventListener("click", e => {
     e.preventDefault();
     isRequired([username, email, password1, password2]);
-    checkLength(username, 4, 13);
-    checkLength(password1, 8, 17);
-    checkEmail(email);
+    lengthCheck(username, 4, 12);
+    lengthCheck(password1, 8, 16);
+    emailCheck(email);
+    passwordMatch(password1, password2);
+    console.log(success.length);
+    if (success.length > 2) {
+        setTimeout(() => {
+            alert("Succesfully registered!");
+            Array.from(success).forEach(el => el.classList.remove("success"));
+            form.reset();
+        }, 500);
+    }
 });
